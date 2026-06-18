7dc27ce746819a75d4b26ce79f9c2476413cbf79
osama noshy <osamanoshey71@gmail.com>
2026-06-18 18:53:05 +0300

    feat: Authorization with Gates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Attendee;
+use App\Models\Event;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('update-event', function ($user, Event $event) {
+            return $user->id === $event->user_id;
+        });
+
+        Gate::define('delete-attendee', function($user, Event $event, Attendee $attendee) {
+            return $user->id === $event->user_id || $user->id === $attendee->user_id;
+        });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AttendeeController;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,10 +10,14 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('events', EventController::class);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
+Route::apiResource('events', EventController::class)
+    ->middleware('auth:sanctum', ['only' => ['store', 'update', 'destroy']]);
+    
 // scoped is a method that allows you to scope the route to the event
 // so that the attendee is always associated with the event
 Route::apiResource('events.attendees', AttendeeController::class)
     // except(['update']) is a method that allows you to exclude the update method from the scoped route
-    ->scoped()->except(['update']);
+    ->scoped()->except(['update'])->middleware('auth:sanctum', ['only' => ['store', 'destroy']]);
